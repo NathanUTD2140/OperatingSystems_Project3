e74495db927c99f71ad00d3af6f3a8ba07cb3a16
NathanUTD2140 <167475701+NathanUTD2140@users.noreply.github.com>
2025-12-10 20:56:22 -0800

    Update and rename project.py to project3.py
    Renamed it to project3.py, didn't realize it was wrong before

--- project3.py
+++ project3.py
@@ -1,14 +1,3 @@
-"""
-project3.py
-Usage examples:
-  python project3.py create test.idx
-  python project3.py insert test.idx 15 100
-  python project3.py search test.idx 15
-  python project3.py load test.idx input.csv
-  python project3.py print test.idx
-  python project3.py extract test.idx output.csv
-"""
-
 import sys
 import os
 import struct
